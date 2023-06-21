d1e8409d5995221fe6193d9662e46a9fc0701818
onishitaisei <97043781+onishitaisei@users.noreply.github.com>
2023-06-22 00:00:42 +0900

    validator

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+  ParseUUIDPipe,
+} from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
   @Get()
-  findAll(): string {
-    return 'This is FindAll';
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id', ParseUUIDPipe) id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): Item {
+    return this.itemsService.create(createItemDto);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id', ParseUUIDPipe) id: string) {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    this.itemsService.delete(id);
   }
 }
